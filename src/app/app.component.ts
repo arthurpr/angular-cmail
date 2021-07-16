cb155590652720d2f1c2583f4280dbe8c808096e
Arthur Freitas <arthurpr0186@gmail.com>
2021-07-16 00:59:16 -0300

    Arquivos Css e Angular Json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titulo = 'cmail';
+  
+
+  private _isNewEmailFormOpen = false;
+
+  get isNewEmailFormOpen(){
+
+    return this._isNewEmailFormOpen;
+  }
+
+  toggleNewEmailForm(){
+
+    this._isNewEmailFormOpen = !this.isNewEmailFormOpen;
+  }
   
 }
